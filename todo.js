06339f647780704c17bbcc6efef2b141d0dec279
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-20 14:49:23 -0600

    add version 4

--- todo.js
+++ todo.js
@@ -1,21 +1,31 @@
 var todoList = {
-  todos: ['task1', 'task2', 'task3'],
+  todos: [],
   displayTodo: function(){
     console.log('My todos:',this.todos);
   },
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodo();
   },
-  changeTodo: function(position, newTodo){
-    this.todos[position] = newTodo;
+  changeTodo: function(position, todoText){
+    this.todos[position].todoText = todoText;
     this.displayTodo();
   },
   deleteTodo: function(position, count){
     this.todos.splice(position, count);
     this.displayTodo();
+  },
+  toggleCompleted: function(position){
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodo();
   }
 };
 
 
-console.log(todoList.deleteTodo(0,1));
+console.log(todoList.addTodo('hello'));
+console.log(todoList.changeTodo(0,'goodbye'));
+console.log(todoList.toggleCompleted(0));
